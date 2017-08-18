4a3a7cf2d6398b05ec62662f7c59c1234c0bf85d
gdd <997470478@qq.com>
2017-08-18 17:26:39 +0800

    样式

--- js/dati.js
+++ js/dati.js
@@ -58,7 +58,10 @@ $(function () {
                 var arr=["一","二","三","四","五","六","七","八","九","十","十一","十二","十三","十四","十五"];
                 num++;
                 $(".ti_txt").html(data.txt);
-                $(".tihao").html(arr[num])
+                $(".tihao").html(arr[num]);
+                if(num==5){
+                    $(".next_ti").attr("href","success.html")
+                }
             },
             error:function(data,status){
                 if(status=='timeout'){
